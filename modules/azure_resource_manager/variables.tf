f9844047057c0a66cebbbd5f90043d1c512efe22
jvtaboada <joao_taboada@hotmail.com>
2025-08-21 18:57:21 -0300

    Adicionado módulo que cria os recursos básicos no Azure

---
+++ modules/azure_resource_manager/variables.tf
@@ -0,0 +1,10 @@
+# Variáveis Azure
+variable "rg_name" {
+  description = "Nome do resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Região onde o resource group será criado"
+  type        = string
+}
