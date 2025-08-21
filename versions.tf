23ee7c670b1f1670ad5d6408bd1ceca23f08f6e2
jvtaboada <joao_taboada@hotmail.com>
2025-08-21 18:58:45 -0300

    Configurando versões dos providers azurerm e fabric

--- versions.tf
+++ versions.tf
@@ -1,1 +1,16 @@
 # Restrições de versão do Terraform e providers
+
+terraform {
+  required_version = ">= 1.12.2, < 2.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    fabric = {
+      source  = "microsoft/fabric"
+      version = "~> 1.0"
+    }
+  }
+}
